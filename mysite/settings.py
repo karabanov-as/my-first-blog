7e36eeefcc4cb33fea9867a773d98bec2c340aa9
karabanov_as <karabanov_as@srvhub.ru>
2020-09-01 13:28:35 +0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_fludkvsk#skjvq5^b567u@-nn6f8_hg&g*)^=&sk*prpnrg36'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ['localhost', '127.0.0.1', '[::1]']
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
